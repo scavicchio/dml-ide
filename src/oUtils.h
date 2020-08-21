0f1b6491b0f4f765326299d37c0a0d764bb58e8f
Salvy Cavicchio <15899724+scavicchio@users.noreply.github.com>
2020-08-20 23:20:41 -0400

    Update oUtils.h

--- src/oUtils.h
+++ src/oUtils.h
@@ -18,6 +18,6 @@
 class oUtils {
 public:
     void static generateMassesPoisson(double minCut, map<Mass *, vector<Spring *>> mToS, vector<Vec> &lattice);
-    void static generateMassesBounded(double minCut, map<Mass *, vector<Spring *>> mToS, vector<Vec> &lattice, int n, model_data *bounding=nullptr);
+    void static generateMassesBounded(double minCut, map<Mass *, vector<Spring *>> mToS, vector<Vec> &lattice, int n);
 
 };
